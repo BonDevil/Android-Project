b0da29ea62a6e4b98276e23a823488495958aacb
Jakub Roszkowski <jakubr1701@gmail.com>
2022-11-30 18:27:08 +0100

    chose_friends

--- app/src/main/java/com/example/anrdoidteamproject/ui/TransferFunds.kt
+++ app/src/main/java/com/example/anrdoidteamproject/ui/TransferFunds.kt
@@ -1,18 +1,142 @@
 package com.example.anrdoidteamproject.ui
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Checkbox
+import androidx.compose.material.Divider
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.anrdoidteamproject.R
+import com.example.anrdoidteamproject.ui.theme.SimpleTextField
 import com.example.anrdoidteamproject.ui.theme.bottomBar
 import com.example.anrdoidteamproject.ui.theme.topBar
+
+
+@Composable
+fun transferFunds() {
+
+    Column(
+        modifier = Modifier
+            .padding(30.dp)
+            .background(Color(24, 31, 54))
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+        )
+    {
+        Text(text = stringResource(R.string.kwota
+        ),
+            color = Color.White,
+            fontSize = 30.sp,)
+        SimpleTextField()
+
+        Text(
+            text = stringResource(R.string.znajomi),
+            color = Color.White,
+            fontSize = 30.sp,
+        )
+        Spacer(modifier = Modifier.height(15.dp))
+
+
+        Divider(color = Color.White, thickness = 2.dp)
+        Listpersons3(SampleData3.conversationSample)
+
+
+
+    }
+}
+
+@Composable
+fun PersonCard3(per: Osoba) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Row() {
+            CheckBoxDemo()
+            Text(
+                text = per.Imie,
+                color = Color.White,
+                fontSize = 25.sp,
+                fontFamily = FontFamily(
+                    Font(R.font.century_gothic)
+                )
+            )
+
+            Spacer(modifier = Modifier.width(4.dp))
+
+            Text(
+                text = per.Nazwisko,
+                color = Color.White,
+                fontSize = 25.sp,
+                fontFamily = FontFamily(
+                    Font(R.font.century_gothic)
+                )
+            )
+            Spacer(modifier = Modifier.width(1.dp))
+
+        }
+        Divider(color = Color.White, thickness = 2.dp)
+
+    }
+
+}
+
+
+@Composable
+fun Listpersons3(osobas: List<Osoba>) {
+    LazyColumn {
+        osobas.map { item { PersonCard3(it) } }
+    }
+}
+
+
+
+
+object SampleData3 {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Osoba(
+            "Jakub",
+            "Roszkowski"
+        ),
+        Osoba(
+            "Piotr",
+            "Grygoruk"
+        ),
+        Osoba(
+            "Nataliia",
+            "Martynenko"
+        ),
+        Osoba(
+            "Adam",
+            "Nowak"
+        ),
+        Osoba(
+            "Jan",
+            "Kowalski"
+        )
+    )
+}
+
+
+
+
 
 
 @Composable
@@ -25,7 +149,7 @@ fun TransferFunds() {
                 .fillMaxHeight(0.925f)
                 .background(color = Color(0xff181f36))
         ) {
-
+            transferFunds()
         }
         bottomBar()
     }

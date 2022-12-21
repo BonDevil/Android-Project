228066a9eeb5bc1bcccd2f5b27604fbc14cb98d5
nMart719 <87510nata@gmail.com>
2022-12-21 18:45:43 +0100

    edited mockups

--- app/src/main/java/com/example/anrdoidteamproject/ui/AddTrip.kt
+++ app/src/main/java/com/example/anrdoidteamproject/ui/AddTrip.kt
@@ -1,7 +1,9 @@
 package com.example.anrdoidteamproject.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -12,6 +14,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,66 +32,119 @@ fun trip_valus() {
 
     Column(
         modifier = Modifier
+
             .background(Color(24, 31, 54))
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
 
         )
     {
+        Column(
+            modifier = Modifier
+                .padding(40.dp)
+                .background(Color(24, 31, 54))
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.Start,
 
-        Text(
-            text = stringResource(R.string.nazwa),
-            color = Color.White,
-            fontSize = 30.sp,
-        )
-        Spacer(modifier = Modifier.height(15.dp))
-        SimpleTextField(
-            KeyboardOptions(
-                keyboardType = KeyboardType.Text,
-                imeAction = ImeAction.Next
-            ),
-        )
-        Spacer(modifier = Modifier.height(20.dp))
-        Text(
-            text = stringResource(R.string.opis),
-            color = Color.White,
-            fontSize = 30.sp,
-        )
-        Spacer(modifier = Modifier.height(15.dp))
-        SimpleTextField(
-            KeyboardOptions(
-                keyboardType = KeyboardType.Text,
-                imeAction = ImeAction.Next
-            ),
-        )
-        Spacer(modifier = Modifier.height(20.dp))
-        Text(
-            text = stringResource(R.string.planowana_kwota),
-            color = Color.White,
-            fontSize = 30.sp,
-        )
-        Spacer(modifier = Modifier.height(15.dp))
-        SimpleTextField(
-            KeyboardOptions(
-                keyboardType = KeyboardType.Number,
-                imeAction = ImeAction.Next
-            ),
-        )
-        Spacer(modifier = Modifier.height(20.dp))
-        Text(
-            text = stringResource(R.string.ilosc_dni),
-            color = Color.White,
-            fontSize = 30.sp,
-        )
-        Spacer(modifier = Modifier.height(15.dp))
-        SimpleTextField(
-            KeyboardOptions(
-                keyboardType = KeyboardType.Number,
-                imeAction = ImeAction.Done
+            ) {
+            Text(
+                text = stringResource(R.string.nazwa),
+                color = Color.White,
+                fontSize = 20.sp,
+                fontFamily = FontFamily(
+                    Font(R.font.century_gothic)
+                ),
             )
-        )
-        Spacer(modifier = Modifier.height(60.dp))
+            Spacer(modifier = Modifier.height(15.dp))
+            TextField(
+                value = "",
+                onValueChange = {},
+//            KeyboardOptions(
+//                keyboardType = KeyboardType.Text,
+//                imeAction = ImeAction.Next
+//            ),
+                modifier = Modifier
+                    .border(2.dp, Color(89, 128, 255), RoundedCornerShape(10))
 
+                    .background(Color(217, 217, 217), RoundedCornerShape(10))
+                    .heightIn(min = 56.dp),
+
+                )
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(
+                text = stringResource(R.string.opis),
+                color = Color.White,
+                fontSize = 20.sp,
+                fontFamily = FontFamily(
+                    Font(R.font.century_gothic)
+                ),
+            )
+            Spacer(modifier = Modifier.height(15.dp))
+            TextField(
+                value = "",
+                onValueChange = {},
+                modifier = Modifier
+                    .border(2.dp, Color(89, 128, 255), RoundedCornerShape(10))
+                    .background(Color(217, 217, 217), RoundedCornerShape(10))
+                    .heightIn(min = 56.dp),
+//
+//            KeyboardOptions(
+//                keyboardType = KeyboardType.Text,
+//                imeAction = ImeAction.Next
+//            ),
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(
+                text = stringResource(R.string.planowana_kwota),
+                color = Color.White,
+                fontSize = 20.sp,
+                fontFamily = FontFamily(
+                    Font(R.font.century_gothic)
+                ),
+            )
+            Spacer(modifier = Modifier.height(15.dp))
+            TextField(
+                value = "",
+                onValueChange = {},
+//            KeyboardOptions(
+//                keyboardType = KeyboardType.Text,
+//                imeAction = ImeAction.Next
+//            ),
+                modifier = Modifier
+                    .fillMaxWidth(0.6f)
+                    .border(2.dp, Color(89, 128, 255), RoundedCornerShape(10))
+                    .background(Color(217, 217, 217), RoundedCornerShape(10))
+                    .heightIn(min = 56.dp),
+
+                )
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(
+                text = stringResource(R.string.ilosc_dni),
+                color = Color.White,
+                fontSize = 20.sp,
+                fontFamily = FontFamily(
+                    Font(R.font.century_gothic)
+                ),
+            )
+            Spacer(modifier = Modifier.height(15.dp))
+
+            TextField(
+                value = "",
+                onValueChange = {},
+//            KeyboardOptions(
+//                keyboardType = KeyboardType.Text,
+//                imeAction = ImeAction.Next
+//            ),
+                modifier = Modifier
+                    .fillMaxWidth(0.3f)
+                    .border(2.dp, Color(89, 128, 255), RoundedCornerShape(10))
+                    .background(Color(217, 217, 217), RoundedCornerShape(10))
+                    .heightIn(min = 56.dp),
+
+
+                )
+            Spacer(modifier = Modifier.height(60.dp))
+        }
     }
 }
 

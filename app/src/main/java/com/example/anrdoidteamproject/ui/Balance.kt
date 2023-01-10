26ba71e06dd4bd9db878d65a0080fef715418171
nMart719 <87510nata@gmail.com>
2023-01-10 23:30:38 +0100

    started Balance ui

--- app/src/main/java/com/example/anrdoidteamproject/ui/Balance.kt
+++ app/src/main/java/com/example/anrdoidteamproject/ui/Balance.kt
@@ -1,20 +1,60 @@
 package com.example.anrdoidteamproject.ui
 
+import android.util.Half.abs
+import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import com.example.anrdoidteamproject.R
 import com.example.anrdoidteamproject.ui.theme.*
 
 
+@Composable
+fun ChartBar(
+    modifier: Modifier = Modifier,
+    percentage: Int,
+    brush: Brush,
+) {
+    Box(
+        modifier = modifier
+            .clip(RectangleShape)
+            .height(kotlin.math.abs(percentage).dp)
+            .width(30.dp)
+            .background(brush)
+            .background(color = Color.Black.copy(alpha = 0.5f))
+    )
+}
+
+@Composable
+private fun GroupLabel(
+    text: String,
+    color: Color = Color(0x40ffffff),
+//    textStyle: TextStyle = MaterialTheme.typography.,
+    isHighlighted: Boolean = false
+) {
+    Text(
+        modifier = Modifier.padding(bottom = 8.dp),
+        text = text,
+        color = color,
+    )
+}
 @Composable
 fun Balance(
     userInfoButtonOnClick: () -> Unit = {},
@@ -46,13 +86,56 @@ fun Balance(
         modifier = Modifier.background(color = Color(0xff181f36))
 
     ) {
-        Row(
+        Column(
             modifier = Modifier
                 .fillMaxWidth()
                 .fillMaxHeight()
                 .background(color = Color(0xff181f36))
         ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.4f)
+                    .background(color = Color(0xffffffff)),
+                verticalAlignment = Alignment.Top
 
+            ) {
+//            Column(
+//                modifier = Modifier
+//                    .fillMaxWidth(0.5f)
+//                    .fillMaxHeight()
+//                    .background(color = Color(0xff000000)),
+//            ) {
+//
+//            }
+//                Column(
+//                    modifier = Modifier
+//                        .fillMaxWidth()
+//                        .fillMaxHeight()
+//                        .background(color = Color(0xffffffff)),
+//                ) {
+//
+//                }
+
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.3f)
+                    .background(color = Color(0xff181f36)),
+                verticalAlignment = Alignment.Bottom
+            ) {
+
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .background(color = Color(0xff000000)),
+                verticalAlignment = Alignment.Bottom
+            ) {
+
+            }
         }
 
     }

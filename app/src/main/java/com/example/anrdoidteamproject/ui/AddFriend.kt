9c7a8ae71f2e85598f5c0e5756ae601fa64df492
BonDevil <grygorukpp@gmail.com>
2023-01-11 18:50:55 +0100

    friends invitations fix

--- app/src/main/java/com/example/anrdoidteamproject/ui/AddFriend.kt
+++ app/src/main/java/com/example/anrdoidteamproject/ui/AddFriend.kt
@@ -33,6 +33,7 @@ fun AddFriend(
     settingsButtonOnClick: () -> Unit = {}
 ) {
     var email by remember { mutableStateOf("") }
+    var isDataRetrieved by remember { mutableStateOf(false) }
 
     Scaffold(
         bottomBar = {
@@ -49,30 +50,23 @@ fun AddFriend(
                 val invitedUserHashedEmail = email.hashCode()
                 val myRef =
                     DatabaseConnection.db.getReference("Users/$invitedUserHashedEmail/friendInvites")
-                var friendsInvites: List<Int>
+                var mySet: MutableSet<Int> = mutableSetOf(currentUserHashedEmail)
 
                 myRef.addListenerForSingleValueEvent(object : ValueEventListener {
                     override fun onDataChange(dataSnapshot: DataSnapshot) {
-                        if (dataSnapshot.value != null) {
-                            friendsInvites = dataSnapshot.value as List<Int>
-//                            val isInvitePresent = friendsInvites.contains(currentUserHashedEmail)
-                            if (currentUserHashedEmail in friendsInvites) {
-                                myRef.setValue(friendsInvites + listOf(currentUserHashedEmail))
-                            }
-//                            Log.d("eo", "$isInvitePresent")
-                            Log.d("eo", "$friendsInvites")
-
-                        } else {
-                            Log.d("eo", "friends invites is null")
-                            myRef.setValue(listOf(currentUserHashedEmail))
-                        }
+                        if(dataSnapshot.value != null)
+                            mySet = (dataSnapshot.value as MutableList<Int>).toMutableSet()
+                        mySet.add(currentUserHashedEmail)
+                        Log.d("eo", "User added to friend invites, $mySet")
+                        isDataRetrieved = true
                     }
-
                     override fun onCancelled(error: DatabaseError) {
                         TODO("Not yet implemented")
                     }
                 })
-
+                if(isDataRetrieved){
+                    myRef.setValue(mySet.toList())
+                }
             })
         },
         modifier = Modifier.background(color = Color(0xff181f36))
@@ -122,7 +116,6 @@ fun AddFriend(
         }
     }
 }
-
 
 @Preview
 @Composable
--- app/src/main/java/com/example/anrdoidteamproject/ui/Invitations.kt
+++ app/src/main/java/com/example/anrdoidteamproject/ui/Invitations.kt
@@ -1,5 +1,6 @@
 package com.example.anrdoidteamproject.ui
 
+import android.util.Log
 import androidx.compose.foundation.background
 
 import androidx.compose.foundation.layout.*
@@ -25,79 +26,89 @@ import androidx.compose.foundation.layout.padding
 
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.runtime.*
 
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import com.example.anrdoidteamproject.businessLogic.DatabaseConnection
+import com.example.anrdoidteamproject.businessLogic.User
 import com.example.anrdoidteamproject.ui.theme.*
-
-
-data class invitation(val Imie: String, val Nazwisko: String)
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CompletableDeferred
 
 
 @Composable
-fun InvitationCard(per: invitation) {
-
+fun InviteCard(
+    inviteInfo: User
+) {
     Column(
         modifier = Modifier
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        PromptButton(
-        label = R.string.akceptuj,
-        onClick = {}
-    )
-        Row(modifier = Modifier.padding(20.dp)) {
-            Text(
-                text = per.Imie,
-                color = Color.White,
-                fontSize = 30.sp,
-                fontFamily = FontFamily(
-                    Font(R.font.century_gothic)
+        Column(modifier = Modifier.padding(10.dp)) {
+            Row() {
+                Text(
+                    text = inviteInfo.firstName,
+                    color = Color.White,
+                    fontSize = 30.sp,
+                    fontFamily = FontFamily(
+                        Font(R.font.century_gothic)
+                    )
                 )
-            )
-
-            Spacer(modifier = Modifier.width(4.dp))
-
-            Text(
-                text = per.Nazwisko,
-                color = Color.White,
-                fontSize = 30.sp,
-                fontFamily = FontFamily(
-                    Font(R.font.century_gothic)
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(
+                    text = inviteInfo.lastName,
+                    color = Color.White,
+                    fontSize = 30.sp,
+                    fontFamily = FontFamily(
+                        Font(R.font.century_gothic)
+                    )
                 )
+            }
+            Spacer(modifier = Modifier.width(5.dp))
+            PromptButton(
+                label = R.string.akceptuj,
+                onClick = {}
             )
-            Spacer(modifier = Modifier.width(1.dp))
-
         }
         Divider(color = Color.White, thickness = 2.dp)
-
-    }
-
-}
-
-
-@Composable
-fun Listpersons(osobas: List<invitation>) {
-    LazyColumn {
-        osobas.map { item { InvitationCard(it) } }
     }
 }
 
+
+//@Composable
+//fun showAllInvites(invites: List<User>) {
+//    LazyColumn {
+//        invites.map { item { InviteCard(it) } }
+//    }
+//    Log.d("eo", "showAllInvites: $invites")
+//
+//}
 
 @Composable
 fun InvitationsList(
     userInfoButtonOnClick: () -> Unit = {},
     homeButtonOnClick: () -> Unit = {},
     settingsButtonOnClick: () -> Unit = {},
-) {
+
+    ) {
     var expanded by remember { mutableStateOf(false) }
+    var friendsInvites = remember { mutableListOf<Int>() }
+    var friendInvitesInfo = remember { mutableListOf<User>() }
+    val currentUserHashedEmail = Firebase.auth.currentUser?.email.hashCode()
+    val myRef =
+        DatabaseConnection.db.getReference("Users/$currentUserHashedEmail/friendInvites")
+    var isDataRetrieved by remember { mutableStateOf(false) }
+    var isFriendListRetrieved by remember { mutableStateOf(false) }
+
     Scaffold(
         bottomBar = {
             bottomBar(
@@ -116,7 +127,47 @@ fun InvitationsList(
                 .fillMaxHeight()
                 .background(color = Color(0xff181f36))
         ) {
-            Listpersons(SampleDataInvitations.conversationSample)
+            myRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    if (dataSnapshot.value != null) {
+                        friendsInvites = (dataSnapshot.value as List<Int>).toMutableList()
+                        Log.d("eo", "friend invites: $friendsInvites")
+                        isDataRetrieved = true
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+            })
+
+            if (isDataRetrieved) {
+                for (hashedFriendMail: Int in friendsInvites) {
+                    val friendRef = DatabaseConnection.db.getReference("Users/$hashedFriendMail")
+                    friendRef.addValueEventListener(object : ValueEventListener {
+                        override fun onDataChange(dataSnapshot: DataSnapshot) {
+                            val myUser = dataSnapshot.getValue(User::class.java)
+                            if (myUser != null) {
+                                friendInvitesInfo =
+                                    (friendInvitesInfo + listOf(myUser)) as MutableList<User>
+                            }
+                            Log.d("eo", "hashedFriendMail loop: $friendInvitesInfo")
+                            isFriendListRetrieved = true
+                        }
+
+                        override fun onCancelled(error: DatabaseError) {
+                            TODO("Not yet implemented")
+                        }
+                    })
+                }
+                if (isFriendListRetrieved) {
+                    LazyColumn {
+                        Log.d("eo", "lazyColumn: $friendInvitesInfo")
+                        friendInvitesInfo.map { item { InviteCard(it) } }
+                    }
+                }
+            }
+
         }
     }
 }
@@ -128,21 +179,6 @@ fun InvitationsListPreview() {
     InvitationsList()
 }
 
-
-object SampleDataInvitations {
-    // Sample conversation data
-    val conversationSample = listOf(
-        invitation(
-            "Jakub",
-            "Roszkowski"
-        ),
-        invitation(
-            "Piotr",
-            "Grygoruk"
-        ),
-
-    )
-}
 
 
 

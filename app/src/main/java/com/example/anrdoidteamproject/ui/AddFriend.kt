68d0fb8303a1f1794179f20a382151155f46511b
Jakub Roszkowski <jakubr1701@gmail.com>
2023-01-13 17:00:19 +0100

    Update AddFriend.kt

--- app/src/main/java/com/example/anrdoidteamproject/ui/AddFriend.kt
+++ app/src/main/java/com/example/anrdoidteamproject/ui/AddFriend.kt
@@ -1,7 +1,7 @@
+
 package com.example.anrdoidteamproject.ui
 
-import android.util.Patterns
-import android.widget.Toast
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
@@ -10,7 +10,6 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
@@ -26,6 +25,7 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.ktx.Firebase
+import kotlin.reflect.typeOf
 
 @Composable
 fun AddFriend(
@@ -35,7 +35,7 @@ fun AddFriend(
 ) {
     var email by remember { mutableStateOf("") }
     var isDataRetrieved by remember { mutableStateOf(false) }
-    var showEmailError by remember { mutableStateOf(false) }
+    var friends = remember { mutableMapOf<String, String>() }
 
     Scaffold(
         bottomBar = {
@@ -48,41 +48,46 @@ fun AddFriend(
         topBar = { topBar(message = stringResource(R.string.dodaj_znajomych)) },
         floatingActionButton = {
             ConfirmButton(confirmOnClick = {
-                if (Patterns.EMAIL_ADDRESS.matcher(email.toString()).matches()) {
+                val currentUserEmail = Firebase.auth.currentUser?.email
+                val currentUserHashedEmail = Firebase.auth.currentUser?.email.hashCode()
+                val invitedUserHashedEmail = email.hashCode()
+                val myRef =
+                    DatabaseConnection.db.getReference("Users/$invitedUserHashedEmail/friendInvites")
 
-                    val currentUserEmail = Firebase.auth.currentUser?.email
-                    val currentUserHashedEmail = Firebase.auth.currentUser?.email.hashCode()
-                    val invitedUserHashedEmail = email.hashCode()
-                    val myRef =
-                        DatabaseConnection.db.getReference("Users/$invitedUserHashedEmail/friendInvites")
+                val checkFriendsRef =
+                    DatabaseConnection.db.getReference("Users/$currentUserHashedEmail/friends")
 
-                    myRef.addListenerForSingleValueEvent(object : ValueEventListener {
-                        override fun onDataChange(dataSnapshot: DataSnapshot) {
-                            if (!currentUserHashedEmail.equals(invitedUserHashedEmail)) {
-                                myRef.child(currentUserHashedEmail.toString())
-                                    .setValue(currentUserEmail.toString())
-                            }
+                checkFriendsRef.addListenerForSingleValueEvent(object : ValueEventListener{
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        if(dataSnapshot.value != null)
+                            friends = dataSnapshot.value as HashMap<String, String>
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        TODO("Not yet implemented")
+                    }
+                })
+
+
+                myRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        if(!currentUserHashedEmail.equals(invitedUserHashedEmail) && !friends.keys.contains(invitedUserHashedEmail.toString())){
+                            myRef.child(currentUserHashedEmail.toString()).setValue(currentUserEmail.toString())
+                            Log.d("eo", "Friend invite successfully sent")
                         }
-
-                        override fun onCancelled(error: DatabaseError) {
-                            TODO("Not yet implemented")
+                        else{
+                            Log.d("eo", "Friend invite not sent, error occured")
                         }
-                    })
-                } else {
-                    showEmailError=true
-                }
+                    }
+                    override fun onCancelled(error: DatabaseError) {
+                        TODO("Not yet implemented")
+                    }
+                })
             })
         },
         modifier = Modifier.background(color = Color(0xff181f36))
 
     ) {
-        if (showEmailError) {
-            Toast.makeText(
-                LocalContext.current, stringResource(R.string.toastCorectValue),
-                Toast.LENGTH_SHORT
-            ).show()
-            showEmailError = false
-        }
         Row(
             modifier = Modifier
                 .fillMaxWidth()
